43c00ff89dfa434a9c75ea9bb8ba4a8a43b3eb54
Hector Climente <hclimente@gmail.com>
2016-10-21 15:33:30 +0200

    syntax corrections

--- scripts/launch_gametesBenchmark.sh
+++ scripts/launch_gametesBenchmark.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#$ -N gametes
 #$ -cwd
 #$ -V
-# launch with qsub -t 1-11 scripts/launch_plink.sh
+#$ -S /bin/sh
 
-# get pedfile
+# generate pedfile
 param_file="populations/gametes/parameters.txt"
 
 h=`head -n$SGE_TASK_ID $param_file | tail -n1 | cut -f1`
@@ -12,12 +13,11 @@ N=`head -n$SGE_TASK_ID $param_file | tail -n1 | cut -f3`
 modelNo=`head -n$SGE_TASK_ID $param_file | tail -n1 | cut -f4`
 repNo=`head -n$SGE_TASK_ID $param_file | tail -n1 | cut -f5`
 
-scripts/gametes2plink $h $maf $N $modelNo $repNo
-
-# run plink
+scripts/gametes2plink.R $h $maf $N $modelNo $repNo
 ped=populations/gametes/h"$h"_maf"$maf"_N"$N"_EDM-"$modelNo"/h"$h"_maf"$maf"_N"$N"_EDM-"$modelNo"_"$repNo".txt.ped
 
+# run plink
 plink --noweb --epistasis --ped $ped --map populations/gametes/map_$N.txt --out $ped.plink --epi1 1 --epi2 1 --allow-no-sex
 
-# launch sixpac
+# run sixpac
 java -jar libs/Sixpac.jar --raw $ped --map populations/gametes/map.txt --out $ped.sixpac --mode complete
