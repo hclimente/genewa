eaf6f9c73f6b74519bf6103a22f521f3974eef85
Hector Climente <hclimente@gmail.com>
2016-11-03 10:03:55 +0100

    fixed beam input generation

--- scripts/ped2beam.R
+++ scripts/ped2beam.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 source("~/libs/wisdom/r/data_analysis_environment.R")
 
 args <- commandArgs(trailingOnly = TRUE)
@@ -6,21 +7,22 @@ beam <- args[2]
 
 snps <- read_tsv(ped, col_names = F) %>% 
   # remove columns related to family, individual, father, mother and sex
-  select(-X1, -X2, -X3, -X4, -X5)
+  select(-X1, -X2, -X3, -X4, -X5) %>%
+  # cases are 1 and controls are 0
+  as.matrix
+snps <- snps - 1L
 
 # fetch phenotype information and remove that column
-phenotypes <- snps$X6 %>% rep(each=2)
-snps <- snps %>% select(-X6)
+phenotypes <- snps[,1] %>% rep(each=2)
+snps <- snps[,-1]
 
 # take separately the two sets of chromosomes
-haplo1 <- snps[,c(T,F)] %>% t
-colnames(haplo1) <- paste0("h1",1:dim(haplo1)[2])
-haplo2 <- snps[,c(F,T)] %>% t
-colnames(haplo2) <- paste0("h2",1:dim(haplo2)[2])
+haplo1 <- snps[,c(T,F)] %>% t %>% as.data.frame %>% set_colnames(., paste0("h1",1:dim(.)[2]))
+haplo2 <- snps[,c(F,T)] %>% t %>% as.data.frame %>% set_colnames(., paste0("h2",1:dim(.)[2]))
 
 # combine the data to create beam format
 intertwinedCols <- paste0(c("h1","h2"),rep(1:dim(haplo2)[2], each=2))
 cbind(haplo1, haplo2) %>%
   .[,intertwinedCols] %>%
   rbind(phenotypes, .) %>%
-  write_tsv(beam)
+  write.table(beam, col.names = F, sep = " ", quote = F, row.names = F)
