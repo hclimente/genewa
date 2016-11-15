20f57168bc252c9c992fe77ab2bdce8f7156b952
Hector Climente <hclimente@gmail.com>
2016-11-15 10:49:51 +0100

    added control for already created populations

--- scripts/gametes.sh
+++ scripts/gametes.sh
@@ -21,5 +21,9 @@ N=`head -n$SGE_TASK_ID $param_file | tail -n1 | cut -f3`
 #####################
 
 gametesOut=populations/gametes/pops/h"$h"_maf"$maf"_N"$N"
-java -jar libs/GAMETES/GAMETES_2.1.jar -M " -h 0.$h -a 0.$maf -a 0.$maf -o $gametesOut" -q 10 -p 1000 -t 100000
-java -jar libs/GAMETES/GAMETES_2.1.jar -i "$gametesOut"_Models.txt -D " -n 0.01 -x 0.5 -a $N -s 1000 -w 1000 -r 100 -o $gametesOut"
+
+# check if one model eg model #10 exists
+if [ ! -d "$gametesOut"_EDM-10 ]; then
+  java -jar libs/GAMETES/GAMETES_2.1.jar -M " -h 0.$h -a 0.$maf -a 0.$maf -o $gametesOut" -q 10 -p 1000 -t 100000
+  java -jar libs/GAMETES/GAMETES_2.1.jar -i "$gametesOut"_Models.txt -D " -n 0.01 -x 0.5 -a $N -s 1000 -w 1000 -r 100 -o $gametesOut"
+fi
