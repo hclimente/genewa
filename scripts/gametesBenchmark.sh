12943d78f3a2c6dcd0f8a55f9c06c4c90b93a781
Hector Climente <hclimente@gmail.com>
2016-12-01 16:25:33 +0100

    added tmp directory
    removed absolute paths

--- scripts/gametesBenchmark.sh
+++ scripts/gametesBenchmark.sh
@@ -26,9 +26,11 @@ unalias rm
 #     GET PARAMS    #
 #####################
 
+baseDir=`pwd`
+
 # number of samples
 n=$1
-param_file="populations/gametes/parameters_long.txt"
+param_file="$baseDir/populations/gametes/parameters_long.txt"
 
 h=`head -n$SGE_TASK_ID $param_file | tail -n1 | cut -d' ' -f1`
 maf=`head -n$SGE_TASK_ID $param_file | tail -n1 | cut -d' ' -f2`
@@ -38,10 +40,10 @@ repNo=`head -n$SGE_TASK_ID $param_file | tail -n1 | cut -d' ' -f5`
 
 # generate PED file
 #######################
-gametesOut=populations/gametes/pops/h"$h"_maf"$maf"_n"$n"_p"$p"
+gametesOut=$baseDir/populations/gametes/pops/h"$h"_maf"$maf"_n"$n"_p"$p"
 gametesFile="$gametesOut"_EDM-"$modelNo"/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".txt
 ped=$gametesFile.ped
-map=populations/gametes/pops/map_$p.txt
+map=$baseDir/populations/gametes/pops/map_$p.txt
 
 # if [ ! -s $ped ];
 # then
@@ -50,7 +52,7 @@ map=populations/gametes/pops/map_$p.txt
 
 # PLINK
 #######################
-plinkOut=populations/gametes/plink/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".plink.txt
+plinkOut=$baseDir/populations/gametes/plink/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".plink.txt
 
 if [ ! -s $plinkOut.epi.cc ];
 then
@@ -59,7 +61,7 @@ fi
 
 # MDR
 #######################
-mdrOut=populations/gametes/mdr/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".mdr.txt
+mdrOut=$baseDir/populations/gametes/mdr/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".mdr.txt
 
 if [ ! -s $mdrOut ];
 then
@@ -74,10 +76,10 @@ fi
 
 # TURF
 #######################
-turfOut=../populations/gametes/turf/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".turf.txt
+turfOut=$baseDir/populations/gametes/turf/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".turf.txt
 
 ## run turf in a box
-box=h"$h"_maf"$maf"_n"$n"_p"$p"_"$modelNo"_$repNo
+box=$baseDir/tmp/h"$h"_maf"$maf"_n"$n"_p"$p"_"$modelNo"_$repNo
 mkdir $box
 cd $box
 
@@ -85,13 +87,13 @@ if [ ! -s $turfOut ];
 then
   if [ "$p" == "20" ]
   then
-    perl ../libs/turf/TuRF-E.pl -f ../$gametesFile -t 20
+    perl $baseDir/libs/turf/TuRF-E.pl -f $gametesFile -t 20
   else
-    perl ../libs/turf/TuRF-E.pl -f ../$gametesFile
+    perl $baseDir/libs/turf/TuRF-E.pl -f $gametesFile
   fi
 
-  perl ../libs/turf/DataConverter/ARFF2MDR.pl SNP_filtered.arff >SNP_filtered.mdr
-  java -jar ../libs/mdr_3.0.2/mdr_3.0.2.jar -min=1 -max=2 -table_data=true SNP_filtered.mdr | \
+  perl $baseDir/libs/turf/DataConverter/ARFF2MDR.pl SNP_filtered.arff >SNP_filtered.mdr
+  java -jar $baseDir/libs/mdr_3.0.2/mdr_3.0.2.jar -min=1 -max=2 -table_data=true SNP_filtered.mdr | \
   ## crop the Top models table
   awk '/### Top Models ###/{f=1} /FINISHED/{f=0;print} f' | \
   ## remove first and last line and blank lines
@@ -100,25 +102,25 @@ then
   sed 's/# /No/' | sed 's/ /_/g' >$turfOut
 fi
 
-cd ..
+cd $baseDir
 rm -r $box
 
 # BEAM
 #######################
 mkdir $box
 
-beamIn=../$gametesFile.beam
-beamRawOut=../populations/gametes/beam/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".beam.raw.txt
-beamOut=../populations/gametes/beam/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".beam.txt
+beamIn=$gametesFile.beam
+beamRawOut=$baseDir/populations/gametes/beam/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".beam.raw.txt
+beamOut=$baseDir/populations/gametes/beam/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".beam.txt
 
 cd $box
 
 if [ ! -s $beamOut ];
 then
-  ../scripts/ped2beam.R ../$ped $beamIn
+  $baseDir/scripts/ped2beam.R $ped $beamIn
 
-  sed "s/INSERT_THIN/$p/" ../libs/beam/parameters.txt >parameters.txt
-  ../libs/beam/BEAM $beamIn $beamRawOut
+  sed "s/INSERT_THIN/$p/" $baseDir/libs/beam/parameters.txt >parameters.txt
+  $baseDir/libs/beam/BEAM $beamIn $beamRawOut
 
   ## take the relevant table
   awk '/DETECTED_ASSOCIATIONS/{f=1} /POSTERIOR_DISTRIBUTION/{f=0;print} f' $beamRawOut | \
@@ -128,13 +130,13 @@ then
   sed 's/( //' | sed 's/ )//' | sed 's/ /,/g' >$beamOut
 fi
 
-cd ..
+cd $baseDir
 rm -r $box
 
 # AntEpiSeeker
 #######################
 aesFile=$gametesFile.aes
-aesOut=../populations/gametes/aes/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".aes.txt
+aesOut=$baseDir/populations/gametes/aes/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".aes.txt
 
 sed 's/\t/,/g' $gametesFile >$aesFile
 
@@ -144,19 +146,19 @@ cd $box
 
 if [ ! -s $aesOut ];
 then
-  sed "s,INPUT_FILE,../$aesFile," ../libs/AntEpiSeeker1.0_linux/parameters.txt |
+  sed "s,INPUT_FILE,$aesFile," $baseDir/libs/AntEpiSeeker1.0_linux/parameters.txt |
   sed "s,OUTPUT_FILE,$aesOut," |
   sed "s,NUM_SNPS,$p," >parameters.txt
 
-  ../libs/AntEpiSeeker1.0_linux/AntEpiSeeker
+  $baseDir/libs/AntEpiSeeker1.0_linux/AntEpiSeeker
 fi
 
-cd ..
+cd $baseDir
 rm -r $box
 
 # GWGGI
 #######################
-gwggiOut=../populations/gametes/gwggi/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".gwggi.txt
+gwggiOut=$baseDir/populations/gametes/gwggi/h"$h"_maf"$maf"_n"$n"_p"$p"_EDM-"$modelNo"_"$repNo".gwggi.txt
 gwggiIn=h"$h"_maf"$maf"_p"$p"_EDM-"$modelNo"_"$repNo"
 
 mkdir $box
@@ -164,12 +166,12 @@ cd $box
 
 if [ ! -s $gwggiOut.tamw.rst ];
 then
-  cut -f1-6 ../$ped >$gwggiIn.ped
-  grep -v ^N ../$gametesFile | sed 's/\t.$//' >$gwggiIn.gen
-  sed 's/$/A\tT/' ../$map >$gwggiIn.map
+  cut -f1-6 $ped >$gwggiIn.ped
+  grep -v ^N $gametesFile | sed 's/\t.$//' >$gwggiIn.gen
+  sed 's/$/A\tT/' $map >$gwggiIn.map
 
-  ../libs/gwggi.unix/gwggi --file $gwggiIn --tamw --converge --ntree 4000 --clsf-sqrt --tree-depth 4 --showntree --out $gwggiOut
+  $baseDir/libs/gwggi.unix/gwggi --file $gwggiIn --tamw --converge --ntree 4000 --clsf-sqrt --tree-depth 4 --showntree --out $gwggiOut
 fi
 
-cd ..
+cd $baseDir
 rm -r $box
