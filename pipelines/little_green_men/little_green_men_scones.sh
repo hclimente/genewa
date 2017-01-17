6734f3668fb2602c99ac66c8cc03c1925f6c896b
Hector Climente <hclimente@gmail.com>
2017-01-17 18:35:20 +0100

    scones run moved to lgm pipeline folder

---
+++ pipelines/little_green_men/little_green_men_scones.sh
@@ -0,0 +1,1 @@
+../scones/run_scones.nf -profile curie -resume --ped ../../data/little_green_men/genotypes.ped --map  ../../data/little_green_men/genotypes.map --snp2gene  ../../data/little_green_men/gene2snp.tsv --icogsSnps ../../data/little_green_men/snp_list.csv --ppi ../../data/little_green_men/ppi.tab
