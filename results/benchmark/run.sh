af602314704ef67d326859ce3953ff5450d2ff42
Hector Climente <hclimente@gmail.com>
2017-10-17 17:23:47 +0200

    updated paths

--- results/benchmark/run.sh
+++ results/benchmark/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-benchmark_from_experiment.nf --geno datasets/genesis/genesis.processed --snp2gene datasets/genesis/gene2snp.hg19 --tab datasets/genesis/BIOGRID-ORGANISM-Homo_sapiens-3.4.138.tab.txt --cases 636 --controls 637 --permutations 10 --rld datasets/genesis/ld.RData --genewawd /data/users/hcliment/projects/genewa --ngenes 20 -resume
+benchmark_from_experiment.nf --geno data/genesis/genesis.processed --snp2gene data/genesis/gene2snp.hg19 --tab data/genesis/BIOGRID-ORGANISM-Homo_sapiens-3.4.138.tab.txt --cases 636 --controls 637 --permutations 10 --rld data/genesis/ld.RData --genewawd /data/users/hcliment/projects/genewa --ngenes 20 -resume
