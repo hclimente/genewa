7794368581fcf108f6ac28f40c9f2696ca24a914
hclimente <hclimente@gmail.com>
2019-04-07 10:49:04 +0200

    added ld pruning step

--- results/preprocessing/biogrid2hgnc.R
+++ results/preprocessing/biogrid2hgnc.R
@@ -6,7 +6,7 @@ library(tidyverse)
 symbols <- read_tsv('non_alt_loci_set.txt') %>%
 	select(entrez_id, symbol)
 
-biogrid <- read_tsv("BIOGRID-MV-Physical-3.5.168.tab2.txt") %>%
+biogrid <- read_tsv("BIOGRID-MV-Physical-3.5.171.tab2.txt") %>%
 	filter(`Organism Interactor A` == 9606 & `Organism Interactor B` == 9606) %>%
 	inner_join(symbols, by = c('Entrez Gene Interactor A' = 'entrez_id')) %>%
 	inner_join(symbols, by = c('Entrez Gene Interactor B' = 'entrez_id')) %>%
