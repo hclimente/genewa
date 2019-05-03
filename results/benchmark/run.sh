8bcaab816dfc9a13d76735cbefde45863e7414b3
hclimente <hclimente@gmail.com>
2019-05-03 14:40:33 +0200

    bugfixes

--- results/benchmark/run.sh
+++ results/benchmark/run.sh
@@ -1,1 +1,1 @@
-../../scripts/benchmark_methods.nf --bfile genesis_2019 --covar CT_age_cens_tronq.cov --snp2gene ../preprocessing/snp2hgnc.tsv --tab2 ../preprocessing/BIOGRID-MV-Physical-3.5.168.tab2.hgnc.tsv -resume
+../../scripts/benchmark_methods.nf --bfile genesis_2019 --covar CT_age_cens_tronq.cov --snp2gene ../preprocessing/snp2hgnc.tsv --tab2 ../preprocessing/BIOGRID-MV-Physical-3.5.171.tab2.hgnc.tsv -profile cluster -resume
