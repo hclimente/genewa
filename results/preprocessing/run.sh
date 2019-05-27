87101e4ce69b9454ecf19dc224e930c63516426c
hclimente <hclimente@gmail.com>
2019-05-27 15:46:05 +0200

    remove covariates

--- results/preprocessing/run.sh
+++ results/preprocessing/run.sh
@@ -8,16 +8,14 @@ impute --bfile genesis_raw_pos_ok --genome GRCh37 --reference 1000GP_Phase3 --st
 # exclude 11 cases
 awk '{print $2,$2}' OFS='\t' listNEW_IND_to_suppress.lst >excluded_samples
 
-# exclude samples and and 20 FGFR2 SNPs and baly genotyped
-plink -bfile out --remove excluded_samples --exclude FGFR2_SNPs_to_exclude.lst --maf 0.001 --mind 0.10 --geno 0.1 --hwe 0.001 --make-bed --out tmp
-plink -bfile tmp -indep-pairwise 50 5 0.75 -out pruned
-plink -bfile tmp -extract pruned.prune.in -make-bed -out filtered
+# exclude samples and and 20 FGFR2 SNPs and badly genotyped
+plink -bfile out --remove excluded_samples --exclude FGFR2_SNPs_to_exclude.lst --maf 0.001 --mind 0.10 --geno 0.1 --hwe 0.001 --make-bed --out filtered
 
 # run vegas
-run_vegas --bfile filtered --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000  -chr 23' --covar CT_age_cens_tronq.cov -resume -profile bigmem
+run_vegas --bfile filtered --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000 -chr 23' -resume -profile bigmem
 mv scored_genes.vegas.txt scored_genes.X.vegas.txt
 
-run_vegas --bfile filtered --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000' --covar CT_age_cens_tronq.cov -resume -profile bigmem
+run_vegas --bfile filtered --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000' -resume -profile bigmem
 tail -n +2 scored_genes.X.vegas.txt >>scored_genes.vegas.txt
 
 rm scored_genes.X.vegas.txt
