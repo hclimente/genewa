c423e357c9bd0b4e5ce501db805589e89ffc0c48
hclimente <hclimente@gmail.com>
2019-05-28 11:27:55 +0200

    removed age-adjustment from sigmod and vegas results

--- results/hotnet/run.sh
+++ results/hotnet/run.sh
@@ -1,1 +1,1 @@
-run_hhotnet --scores ../preprocessing/scored_genes.vegas.txt --tab2 ../preprocessing/BIOGRID-MV-Physical-3.5.171.tab2.hgnc.tsv --hhnet_path hierarchical-hotnet -profile cluster -resume
+run_hhotnet --scores ../preprocessing/scored_genes.vegas.txt --tab2 ../preprocessing/BIOGRID-MV-Physical-3.5.171.tab2.hgnc.tsv --hhnet_path hierarchical-hotnet/src -profile cluster -resume
