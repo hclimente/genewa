0e3cbbc1c438d39c6dfbdd2e4b22376d8204909b
hclimente <hclimente@gmail.com>
2019-05-28 15:54:31 +0200

    iadded heinz results, with fdr 0.5

--- results/heinz/run.sh
+++ results/heinz/run.sh
@@ -1,1 +1,1 @@
-run_heinz --vegas ../preprocessing/scored_genes.vegas.txt --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt -profile bigmem -resume 
+run_heinz --vegas ../preprocessing/scored_genes.vegas.txt --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt --fdr 0.5 -profile bigmem -resume 
