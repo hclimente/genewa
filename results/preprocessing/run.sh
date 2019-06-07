72940afabb540ce441ae4f956b22c282b1472e4f
hclimente <hclimente@gmail.com>
2019-06-07 15:43:53 +0200

    decided to remove imputation altogether

--- results/preprocessing/run.sh
+++ results/preprocessing/run.sh
@@ -2,26 +2,23 @@
 awk '$4 == -1 {print $2}' genesis_raw.bim >unknown_pos
 plink --bfile genesis_raw --exclude unknown_pos --make-bed --out genesis_raw_pos_ok
 
-# impute
-impute --bfile genesis_raw_pos_ok --genome GRCh37 --reference 1000GP_Phase3 --strand_info strand_info --population EUR -resume -profile bigmem
-
 # exclude 11 cases
 awk '{print $2,$2}' OFS='\t' listNEW_IND_to_suppress.lst >excluded_samples
 
 # exclude samples and and 20 FGFR2 SNPs and badly genotyped
-plink -bfile out --remove excluded_samples --exclude FGFR2_SNPs_to_exclude.lst --maf 0.001 --mind 0.10 --geno 0.1 --hwe 0.001 --make-bed --out filtered
+plink -bfile genesis_raw_pos_ok --remove excluded_samples --exclude FGFR2_SNPs_to_exclude.lst --maf 0.001 --mind 0.10 --geno 0.1 --hwe 0.001 --make-bed --out genesis_2019 
 
 # run vegas
-run_vegas --bfile filtered --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000 -chr 23' -resume -profile bigmem
+run_vegas --bfile genesis_2019 --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000 -chr 23' -resume -profile bigmem
 mv scored_genes.vegas.txt scored_genes.X.vegas.txt
 
-run_vegas --bfile filtered --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000' -resume -profile bigmem
+run_vegas --bfile genesis_2019 --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000' -resume -profile bigmem
 tail -n +2 scored_genes.X.vegas.txt >>scored_genes.vegas.txt
 
 rm scored_genes.X.vegas.txt
 
 # map snps to genes
-snp2gene --bim filtered.bim --genome GRCh37 -profile cluster
+snp2gene --bim genesis_2019.bim --genome GRCh37 -profile cluster
 
 # map BIOGRID to HGNC based on Entrez Ids
 ./biogrid2hgnc.R
