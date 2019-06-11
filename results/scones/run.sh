2b03ec661f71b666915ec2589a35601e49f622a3
hclimente <hclimente@gmail.com>
2019-06-11 14:14:59 +0200

    updated results without age adjustment

--- results/scones/run.sh
+++ results/scones/run.sh
@@ -1,3 +1,3 @@
-run_scones --bfile filtered --network gs -resume -profile cluster && mv cones.tsv cones_gs.tsv
-run_scones --bfile filtered --network gm --snp2gene ../preprocessing/snp2hgnc.tsv -profile bigmem -resume && mv cones.tsv cones_gm.tsv
-run_scones --bfile filtered --network gi --snp2gene ../preprocessing/snp2hgnc.tsv --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt -profile bigmem -resume && mv cones.tsv cones_gi.tsv
+run_scones --bfile ../preprocessing/genesis_2019 --network gs -resume -profile cluster && mv cones.tsv cones_gs.tsv
+run_scones --bfile ../preprocessing/genesis_2019 --network gm --snp2gene ../preprocessing/snp2hgnc.tsv -profile bigmem -resume && mv cones.tsv cones_gm.tsv
+run_scones --bfile ../preprocessing/genesis_2019 --network gi --snp2gene ../preprocessing/snp2hgnc.tsv --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt -profile bigmem -resume && mv cones.tsv cones_gi.tsv
