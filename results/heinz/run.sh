114b97fb3e445a4a964245ba8090db1a57ecdc36
hclimente <hclimente@gmail.com>
2019-06-21 10:48:56 +0200

    updated results using top10% SNP p-value

--- results/heinz/run.sh
+++ results/heinz/run.sh
@@ -1,1 +1,2 @@
-run_heinz --vegas ../preprocessing/scored_genes.vegas.txt --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt --fdr 0.5 -profile bigmem -resume 
+cut -f2,9 ../preprocessing/scored_genes.vegas.txt | sed 's/Top-0.1-pvalue/Pvalue/' >scored_genes.top10.txt
+run_heinz --vegas scored_genes.top10.txt --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt --fdr 0.5 -profile bigmem -resume 
--- results/sigmod/run.sh
+++ results/sigmod/run.sh
@@ -1,2 +1,3 @@
 #wget https://github.com/YuanlongLiu/SigMod/raw/master/SigMod_v2.zip && unzip SigMod_v2.zip
-run_sigmod --sigmod SigMod_v2 --vegas ../preprocessing/scored_genes.vegas.txt --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt -profile bigmem -resume 
+cut -f2,9 ../preprocessing/scored_genes.vegas.txt | sed 's/Top-0.1-pvalue/Pvalue/' >scored_genes.top10.txt
+run_sigmod --sigmod SigMod_v2 --vegas scored_genes.top10.txt --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt -profile bigmem -resume 
