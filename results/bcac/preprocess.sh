c696b6f53fac5a8839f61bee104e95660bd305d8
hclimente <hclimente@gmail.com>
2019-06-28 18:57:30 +0200

    updated to use only icogs snps

--- results/bcac/preprocess.sh
+++ results/bcac/preprocess.sh
@@ -3,4 +3,4 @@ echo 'CHR\tSNP\tBP\tA1\tF_A\tF_U\tA2\tCHISQ\tP\tOR' >icogs_bcac_public_results_e
 tail -n +2 ~/data/bcac/icogs_bcac_public_results_euro.txt | awk 'BEGIN { OFS = "\t" }{print $2, $6, $3, $4, $9, $9, $5, $14, $15, $13}' | grep -v NULL | sed 's/:[^\t]\+//' >>icogs_bcac_public_results_euro.assoc
 
 # select only genesis snps
-R -e 'library(tidyverse); snps <- read_tsv("~/data/genesis/genesis_2019.bim", col_names = FALSE)$X2; read_tsv("icogs_bcac_public_results_euro.assoc") %>% filter(SNP %in% snps) %>% write_tsv("icogs_bcac_public_results_euro.genesis.assoc")'
+R -e 'library(tidyverse); snps <- read_tsv("~/data/genesis/genesis_2019.bim", col_names = FALSE) %>% rename(CHR = X1, SNP = X2, BP = X4) %>% select(CHR, SNP, BP); read_tsv("icogs_bcac_public_results_euro.assoc") %>% select(-SNP) %>% inner_join(snps) %>% select(CHR,SNP,BP,A1,F_A,F_U,A2,CHISQ,P,OR) %>% write_tsv("icogs_bcac_public_results_euro.genesis.assoc")'
