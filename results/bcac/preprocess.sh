d6b428fdefc6ec37afcd2f5685d1fe31a1d33327
hclimente <hclimente@gmail.com>
2019-07-09 11:27:51 +0200

    added vegas scores for bcac

--- results/bcac/preprocess.sh
+++ results/bcac/preprocess.sh
@@ -1,5 +1,5 @@
 # make plink output
-echo 'CHR\tSNP\tBP\tA1\tF_A\tF_U\tA2\tCHISQ\tP\tOR' >icogs_bcac_public_results_euro.assoc
+echo -e 'CHR\tSNP\tBP\tA1\tF_A\tF_U\tA2\tCHISQ\tP\tOR' >icogs_bcac_public_results_euro.assoc
 tail -n +2 ~/data/bcac/icogs_bcac_public_results_euro.txt | awk 'BEGIN { OFS = "\t" }{print $2, $6, $3, $4, $9, $9, $5, $14, $15, $13}' | grep -v NULL | sed 's/:[^\t]\+//' >>icogs_bcac_public_results_euro.assoc
 
 # select only genesis snps
