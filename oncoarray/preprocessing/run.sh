8d6f0373078c1e87d36b18caa3934e6f5bfab4ea
hclimente <hclimente@gmail.com>
2019-07-26 09:53:48 +0200

    compute eigenvectors on bcac

--- oncoarray/preprocessing/run.sh
+++ oncoarray/preprocessing/run.sh
@@ -2,3 +2,6 @@
 plink -bfile dbgap_bcac_oncoarray_c8 --maf 0.05 -indep-pairwise 50 5 0.1 -out indep_snps
 plink -bfile dbgap_bcac_oncoarray_c8 -extract indep_snps.prune.in -make-bed
 plink -bfile plink --pca
+
+mv plink.eigenvec bcac_maf0.05_ld0.1.eigenvec
+mv plink.eigenval bcac_maf0.05_ld0.1.eigenval
