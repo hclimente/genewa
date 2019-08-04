b120c7c9488a488b78200ba746a017f35e8caa26
hclimente <hclimente@gmail.com>
2019-08-04 10:04:03 +0200

    updated conventional gwas with proper vegas mapping

--- results/conventional_gwas/run.sh
+++ results/conventional_gwas/run.sh
@@ -16,20 +16,20 @@ plink --bfile genesis_2019 -indep-pairwise 50 5 0.75 -out pruned
 plink --bfile genesis_2019 -extract pruned.prune.in -make-bed -out genesis_2019_pruned
 
 # run vegas
-run_vegas --bfile genesis_2019_pruned --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000 -chr 23' -resume -profile bigmem
+run_vegas --bfile genesis_2019_pruned --genome 37 --vegas_params '-top 10 -upper 50000 -lower 50000 -chr 23' -resume -profile bigmem
 mv scored_genes.vegas.txt scored_genes.X.vegas.txt
 
-run_vegas --bfile genesis_2019_pruned --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000' -resume -profile bigmem
+run_vegas --bfile genesis_2019_pruned --genome 37 --vegas_params '-top 10 -upper 50000 -lower 50000' -resume -profile bigmem
 tail -n +2 scored_genes.X.vegas.txt >>scored_genes.vegas.txt
 
 rm scored_genes.X.vegas.txt
 mv scored_genes.vegas.txt scored_genes.vegas.pruned.txt
 
 # run vegas
-run_vegas --bfile genesis_2019 --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000 -chr 23' --covar CT_age_cens_tronq.cov -resume -profile bigmem
+run_vegas --bfile genesis_2019 --genome 37 --vegas_params '-top 10 -upper 50000 -lower 50000 -chr 23' --covar CT_age_cens_tronq.cov -resume -profile bigmem
 mv scored_genes.vegas.txt scored_genes.X.vegas.txt
 
-run_vegas --bfile genesis_2019 --genome GRCh37 --vegas_params '\-top 10 -upper 50000 -lower 50000' --covar CT_age_cens_tronq.cov -resume -profile bigmem
+run_vegas --bfile genesis_2019 --genome 37 --vegas_params '-top 10 -upper 50000 -lower 50000' --covar CT_age_cens_tronq.cov -resume -profile bigmem
 tail -n +2 scored_genes.X.vegas.txt >>scored_genes.vegas.txt
 
 rm scored_genes.X.vegas.txt
--- results/conventional_gwas/vegas_5p/run.sh
+++ results/conventional_gwas/vegas_5p/run.sh
@@ -1,8 +1,7 @@
-run_vegas --bfile genesis_2019 --genome GRCh37 --vegas_params '\-top 5 -upper 50000 -lower 50000 -chr 23' -resume -profile bigmem
+run_vegas --bfile genesis_2019 --genome 37 --vegas_params '-top 5 -upper 50000 -lower 50000 -chr 23' -resume -profile bigmem
 mv scored_genes.vegas.txt scored_genes.X.vegas.txt
 
-run_vegas --bfile genesis_2019 --genome GRCh37 --vegas_params '\-top 5 -upper 50000 -lower 50000' -resume -profile bigmem
+run_vegas --bfile genesis_2019 --genome 37 --vegas_params '-top 5 -upper 50000 -lower 50000' -resume -profile bigmem
 tail -n +2 scored_genes.X.vegas.txt >>scored_genes.vegas.txt
 
 rm scored_genes.X.vegas.txt
-mv scored_genes.vegas.txt scored_genes.vegas.txt
