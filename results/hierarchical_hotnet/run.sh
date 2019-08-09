b5208fec7536ffa89e0f4557a7012a633962c8d0
hclimente <hclimente@gmail.com>
2019-08-09 10:17:34 +0200

    removed hierarchical hotnet analyses

--- results/hierarchical_hotnet/run.sh
+++
@@ -1,2 +0,0 @@
-cut -f2,9 ../preprocessing/scored_genes.vegas.txt | sed 's/Top-0.1-pvalue/Pvalue/' >scored_genes.top10.txt
-run_hhotnet --scores scored_genes.top10.txt --tab2 ../preprocessing/BIOGRID-ORGANISM-Homo_sapiens-3.5.172.tab2.hgnc.txt --hhnet_path hierarchical-hotnet/src -profile cluster -resume
