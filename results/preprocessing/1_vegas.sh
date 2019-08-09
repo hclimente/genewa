0cf129a44a6932b4a78288959afb746c12fb3408
hclimente <hclimente@gmail.com>
2019-08-09 09:22:07 +0200

    vegas results fixed with no duplications and correct coordinates

--- results/preprocessing/1_vegas.sh
+++ results/preprocessing/1_vegas.sh
@@ -1,8 +1,8 @@
-# run vegas
-run_vegas --bfile genesis_2019 --gencode 31 --genome 37 --vegas_params '-top 10 -upper 50000 -lower 50000 -chr 23' -resume -profile bigmem
+vegas2.nf --bfile genesis_2019 --gencode 31 --genome 37 --buffer 50000 --vegas_params '-top 10 -chr 23' -resume -profile bigmem
 mv scored_genes.vegas.txt scored_genes.X.vegas.txt
 
-run_vegas --bfile genesis_2019 --gencode 31 --genome 37 --vegas_params '-top 10 -upper 50000 -lower 50000' -resume -profile bigmem
+vegas2.nf --bfile genesis_2019 --gencode 31 --genome 37 --buffer 50000 --vegas_params '-top 10' -resume -profile bigmem
 tail -n +2 scored_genes.X.vegas.txt >>scored_genes.vegas.txt
 
 rm scored_genes.X.vegas.txt
+../../scripts/ensembl2hgnc.R scored_genes.vegas.txt
