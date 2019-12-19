af6d78666530495e5b184309165ff8b8dcdf5c45
hclimente <hclimente@gmail.com>
2019-12-19 17:07:35 +0100

    updated bcac with oncoarray dataset

--- results/bcac/run.sh
+++ results/bcac/run.sh
@@ -1,2 +1,2 @@
-vegas2.nf --snp_association icogs_bcac_public_results_euro.genesis.assoc.vegas --ld_controls g1000p3_EUR --genome 37 --gencode 31 --buffer 50000 --vegas_params '-top 10' -resume -profile bigmem
+vegas2.nf --snp_association oncoarray_bcac_public_release_oct17.assoc --ld_controls g1000p3_EUR --genome 37 --gencode 31 --buffer 50000 --vegas_params '-top 10' -resume -profile bigmem
 ../../scripts/ensembl2hgnc.R scored_genes.vegas.txt ../preprocessing/non_alt_loci_set.txt
