16c69e2a7acd78186af21cc0adc0804e90a8c54c
hclimente <hclimente@gmail.com>
2019-12-22 08:54:46 +0100

    updated bcac results using oncoarray as well

--- results/preprocessing/1_vegas.sh
+++ results/preprocessing/1_vegas.sh
@@ -1,8 +1,2 @@
-vegas2.nf --bfile genesis_2019 --gencode 31 --genome 37 --buffer 50000 --vegas_params '-top 10 -chr 23' -resume -profile bigmem
-mv scored_genes.vegas.txt scored_genes.X.vegas.txt
-
 vegas2.nf --bfile genesis_2019 --gencode 31 --genome 37 --buffer 50000 --vegas_params '-top 10' -resume -profile bigmem
-tail -n +2 scored_genes.X.vegas.txt >>scored_genes.vegas.txt
-
-rm scored_genes.X.vegas.txt
-../../scripts/ensembl2hgnc.R scored_genes.vegas.txt ../preprocessing/non_alt_loci_set.txt
+../../scripts/ensembl2hgnc.R scored_genes.vegas.txt non_alt_loci_set.txt
--- scripts/ensembl2hgnc.R
+++ scripts/ensembl2hgnc.R
@@ -3,7 +3,7 @@
 args = commandArgs(trailingOnly=TRUE)
 
 library(tidyverse);
-ensembl2hgnc <- read_tsv(args[2]) %>%
+ensembl2hgnc <- read_tsv(args[2], col_types = cols(.default = "c")) %>%
     select(symbol, ensembl_gene_id)
 vegas <- read_tsv(args[1], col_types = 'iciddddddcd') %>%
     inner_join(ensembl2hgnc, by = c('Gene' = 'ensembl_gene_id')) %>%
