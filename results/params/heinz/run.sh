d014dda48d58cb8ca4890dd50c243961135e397f
hclimente <hclimente@gmail.com>
2020-08-27 17:22:44 +0200

    start parametrization comparison

---
+++ results/params/heinz/run.sh
@@ -0,0 +1,6 @@
+cut -f2,9 ../../preprocessing/scored_genes.vegas.txt | sed 's/Top-0.1-pvalue/Pvalue/' >scored_genes.top10.txt
+for i in `seq 0 0.05 1`
+do
+  heinz.nf --vegas scored_genes.top10.txt --tab2 ../../preprocessing/hint.ht_complex.hgnc.pseudo.tab2 --fdr $i -profile bigmem -resume
+  mv selected_genes.heinz.txt selected_genes.heinz.$i.txt
+done
